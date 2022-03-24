5fbdcf55515985851ddcf7d9c3b71c8c39794d7e
John Ryan Prescott <johnryanprescott@gmail.com>
2022-03-23 17:15:51 -0700

    adding variables for string literals

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,20 @@
 set -ex
 
 CLONE_DIR=$(mktemp -d)
+FW_STRING="FW"
+BOOT_STRING="BOOTLOADER"
 
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.email "$INPUT_USER_NAME"
 git clone --single-branch --branch main "https://x-access-token:$API_TOKEN_GITHUB@github.com/unspun/fw-vega.git" "$CLONE_DIR"
 
-if [[ "$INPUT_TYPE" == "BOOTLOADER" ]]
+if [[ "$INPUT_TYPE" == "$BOOT_STRING" ]]
 then
+  echo "Copying over bootloader resources"
   cp -R "fw_upload" "$CLONE_DIR"
   cp "upload_all" "$CLONE_DIR"
   cp "upload_manifest.template.json" "$CLONE_DIR"
-elif [[ "$INPUT_TYPE" == "FW" ]]
+elif [[ "$INPUT_TYPE" == "$FW_STRING" ]]
 then
   rm -rf "$INPUT_BASE_ARTIFACT-*"
   echo "Adding files: $INPUT_SOURCE_FILE_MFG & $INPUT_SOURCE_FILE_UPLOAD"
